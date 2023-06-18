9522ba00e8ca98b2ca1d87b30737e00c82517494
Takahiro Hirakawa <make.bonjiri@gmail.com>
2023-06-18 17:43:43 +0900

    add Commonly used web scraping code

---
+++ common_web_scraping.py
@@ -0,0 +1,33 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+
+
+class UseBeautifulSoup:
+    def __init__(self, url):
+        self.url = url
+
+    def get_soup(self) -> BeautifulSoup:
+        response = requests.get(self.url)
+        soup = BeautifulSoup(response.text, 'html.parser')
+        return soup
+
+    def display_html_layers(self, soup: BeautifulSoup) -> None:
+        print(soup.prettify())
+
+
+# plese set URL
+URL = ''
+
+use_beautiful_soup = UseBeautifulSoup(URL)
+tag = 'dd ul li p a.TextLinkWrapper_t1bvfs58'
+elements = use_beautiful_soup.get_soup().select(tag)
+
+computer_science_classes = []
+for element in elements:
+    class_name = element.get_text(strip=True)
+    computer_science_classes.append(class_name)
+
+df = pd.DataFrame(computer_science_classes)
+csv_file_name = ''
+df.to_csv(csv_file_name, index=False, header=False)
